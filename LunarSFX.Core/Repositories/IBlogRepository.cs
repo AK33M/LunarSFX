0eeccd3ce962ddc3090bdb9d393ed14dfb5c7773
Akeem Taiwo (Home) <akeemtaiwo@outlook.com>
2016-05-27 13:40:37 +0100

    Added FluentNHibernate Map Classses and Repository

---
+++ LunarSFX.Core/Repositories/IBlogRepository.cs
@@ -0,0 +1,15 @@
+using LunarSFX.Core.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunarSFX.Core.Repositories
+{
+    public interface IBlogRepository
+    {
+        IList<Post> Posts(int pageNo, int pageSize);
+        int TotalPosts();
+    }
+}
