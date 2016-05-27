482f7dc8abc2d473fadd8444ef30b40b6af97584
Akeem Taiwo (Home) <akeemtaiwo@outlook.com>
2016-05-27 14:05:30 +0100

    Added Ninject, Ninject.Mvc5, Minject.WebApi

--- LunarSFX/Global.asax.cs
+++ LunarSFX/Global.asax.cs
@@ -7,17 +7,31 @@ using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 using System.Web.Http;
+using Ninject.Web.Common;
+using Ninject;
+using LunarSFX.Core.Repositories;
 
 namespace LunarSFX
 {
-    public class Global : HttpApplication
+    public class Global : NinjectHttpApplication
     {
-        void Application_Start(object sender, EventArgs e)
+        protected override IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+
+            kernel.Load(new RepositoryModule());
+            kernel.Bind<IBlogRepository>().To<BlogRepository>();
+
+            return kernel;
+        }
+
+        protected override void OnApplicationStarted()
         {
             // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            base.OnApplicationStarted();
         }
     }
 }
