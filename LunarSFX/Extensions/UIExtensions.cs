9ec7742c5e0836b8cc3c5e9a5e2c490dc8021de8
Akeem Taiwo (Home) <akeemtaiwo@outlook.com>
2016-05-27 15:23:19 +0100

    completed uiextensions and partial views

---
+++ LunarSFX/Extensions/UIExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace LunarSFX.Extensions
+{
+    public static class UIExtensions
+    {
+        public static string ToConfigLocalTime(this DateTime utcDT)
+        {
+            var istTZ = TimeZoneInfo.FindSystemTimeZoneById(ConfigurationManager.AppSettings["Timezone"]);
+            return string.Format("{0} ({1})", TimeZoneInfo.ConvertTimeFromUtc(utcDT, istTZ).ToShortDateString(), ConfigurationManager.AppSettings["TimezoneAbbr"]);
+        }
+    }
+}
