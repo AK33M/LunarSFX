64f329321cdabdeb15f5972c8ad411454702c040
Akeem Taiwo (Home) <akeemtaiwo@outlook.com>
2016-06-03 02:09:55 +0100

    more UI changes

--- LunarSFX/Extensions/ActionLinkExtensions.cs
+++ LunarSFX/Extensions/ActionLinkExtensions.cs
@@ -25,23 +25,23 @@ namespace LunarSFX.Extensions
         public static MvcHtmlString CategoryLink(this HtmlHelper helper,
             Category category)
         {
-            return helper.ActionLink(category.Name, "Category", "Blog",
+            return helper.ActionLink(string.Format("@{0}",category.Name), "Category", "Blog",
                 new
                 {
                     category = category.UrlSlug
                 },
                 new
                 {
-                    title = String.Format("See all posts in {0}", category.Name)
+                    title = string.Format("See all posts in {0}", category.Name)
                 });
         }
 
         public static MvcHtmlString TagLink(this HtmlHelper helper, Tag tag)
         {
-            return helper.ActionLink(tag.Name, "Tag", "Blog", new { tag = tag.UrlSlug },
+            return helper.ActionLink(string.Format("#{0} ", tag.Name), "Tag", "Blog", new { tag = tag.UrlSlug },
                 new
                 {
-                    title = String.Format("See all posts in {0}", tag.Name)
+                    title = string.Format("See all posts in {0}", tag.Name)
                 });
         }
     }
