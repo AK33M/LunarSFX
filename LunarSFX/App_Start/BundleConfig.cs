b6a7fd357a5f9ea65f2ec50a0bce3818d57f91a0
Akeem Taiwo (Home) <akeemtaiwo@outlook.com>
2016-06-17 17:18:11 +0100

    installed jqGrid manually!

--- LunarSFX/App_Start/BundleConfig.cs
+++ LunarSFX/App_Start/BundleConfig.cs
@@ -17,6 +17,12 @@ namespace LunarSFX
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                        "~/Scripts/jquery-ui-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqgrid").Include(
+                        "~/Scripts/jquery.jqGrid-5.1.1/js/i18n/grid.locale-en.js",
+                        "~/Scripts/jquery.jqGrid-5.1.1/js/jquery.jqGrid.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/ui.jqGrid/css").IncludeDirectory("~/Scripts/jquery.jqGrid-5.1.1/css", "*.css"));
+
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
               "~/Content/themes/base/all.css"));
         }
