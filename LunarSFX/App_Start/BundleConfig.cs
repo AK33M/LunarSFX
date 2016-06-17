540464d2169c0f3e08adb1661b86411a740e61b9
Akeem Taiwo (Home) <akeemtaiwo@outlook.com>
2016-06-17 13:42:19 +0100

    tinymce js

--- LunarSFX/App_Start/BundleConfig.cs
+++ LunarSFX/App_Start/BundleConfig.cs
@@ -12,7 +12,7 @@ namespace LunarSFX
             bundles.Add(new StyleBundle("~/Content/clean/css").IncludeDirectory("~/Content/themes/clean", "*.css"));
 
             //New bundling
-            bundles.Add(new ScriptBundle("~/bundles/jquery/js").Include("~/Scripts/js/jquery.js", "~/Scripts/jqueryui/jquery-ui.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery/js").Include("~/Scripts/js/jquery.js", "~/Scripts/jqueryui/jquery-ui.js", "~/Scripts/tinymce/jquery.tinymce.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/jqueryui/css").IncludeDirectory("~/Content/styles/jqueryui", "*.css"));
             bundles.Add(new StyleBundle("~/Content/jqueryui/custom/css").IncludeDirectory("~/Content/sunny", "*.css"));
@@ -21,7 +21,7 @@ namespace LunarSFX
 
             bundles.Add(new StyleBundle("~/Content/jqgrid/css").IncludeDirectory("~/Content/styles/jqgrid", "*.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/tinymce/js").Include("~/Scripts/tinymce/jquery.tinymce.min.js", "~/Scripts/tinymce/tinymce.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/tinymce/js").Include("~/Scripts/tinymce/tinymce.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/admin/css").IncludeDirectory("~/Content/admin", "*.css"));
 
