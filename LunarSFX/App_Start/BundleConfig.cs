e82965f4f5ae1f45b6cba289b54dad08dad69e8f
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2016-06-21 15:14:56 +0100

    fixed messy bundling

--- LunarSFX/App_Start/BundleConfig.cs
+++ LunarSFX/App_Start/BundleConfig.cs
@@ -6,21 +6,23 @@ namespace LunarSFX
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/js/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/js").IncludeDirectory("~/Scripts", "*.js", true));
-            bundles.Add(new StyleBundle("~/Content/clean/css").IncludeDirectory("~/Content/themes/clean", "*.css"));
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/clean-blog/css").Include("~/Content/themes/clean/clean-blog.css"));
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css",
+                                                                        "~/Content/bootstrap-theme.css"));
+            bundles.Add(new ScriptBundle("~/Scripts/js").Include("~/Scripts/clean-blog.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/bootstrap").Include("~/Scripts/bootstrap.js"));
 
             //Admin bundling
+            bundles.Add(new StyleBundle("~/Content/ui.jqGrid/css").IncludeDirectory("~/Scripts/jquery.jqGrid-5.1.1/css", "*.css"));
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include("~/Content/themes/base/all.css"));
+            bundles.Add(new StyleBundle("~/Content/admin/css").Include("~/Content/admin/admin.css"));
+
             bundles.Add(new ScriptBundle("~/Scripts/jquery").Include("~/Scripts/jquery-{version}.js"));
             bundles.Add(new ScriptBundle("~/Scripts/jqueryui").Include("~/Scripts/jquery-ui-{version}.js"));
             bundles.Add(new ScriptBundle("~/Scripts/jqgrid").Include("~/Scripts/jquery.jqGrid-5.1.1/js/i18n/grid.locale-en.js",
                                                                      "~/Scripts/jquery.jqGrid-5.1.1/js/jquery.jqGrid.min.js"));
-            bundles.Add(new StyleBundle("~/Content/ui.jqGrid/css").IncludeDirectory("~/Scripts/jquery.jqGrid-5.1.1/css", "*.css"));
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include("~/Content/themes/base/all.css"));
             bundles.Add(new ScriptBundle("~/Scripts/tinymce/js").Include("~/Scripts/tinymce/tinymce.js"));
-            bundles.Add(new StyleBundle("~/Content/admin/css").Include("~/Content/admin/admin.css"));
             bundles.Add(new ScriptBundle("~/Scripts/admin/js").Include("~/Scripts/admin.js"));
         }
     }
